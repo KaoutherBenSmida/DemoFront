c7c98c8abeea38f292b02b58b592106c98b8e71d
Kaouther Ben Smida <kaouther.bensmida@gmail.com>
2022-11-16 21:01:35 +0100

    commit depuis k

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'home/User',
+    component: UserComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ 
+
+
+ }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Test';
+  title = 'Test app';
+  constructor(
+    private router: Router,
+  ) {}
+  goToUser(){
+    this.router.navigate(['home/User']);
+    console.log("hello")
+  
+  }
+
+
 }
+
+
